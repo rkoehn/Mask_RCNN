700354a2e5d95bc7f81825be9caab07709eb5b40
Reinhard Koehn <reinhardkoehn@gmail.com>
2019-08-11 19:04:29 +0200

    Turn off multiprocessing to save memory

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2361,6 +2361,10 @@ class MaskRCNN():
         else:
             workers = multiprocessing.cpu_count()
 
+        # TEST	
+        workers = 0
+	
+
         self.keras_model.fit_generator(
             train_generator,
             initial_epoch=self.epoch,
@@ -2370,8 +2374,8 @@ class MaskRCNN():
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=1,
-            use_multiprocessing=False,
+            workers=workers,
+            use_multiprocessing=True,
         )
         self.epoch = max(self.epoch, epochs)
 
